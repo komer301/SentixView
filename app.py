3cc9754aafb59ba9fa4c709cf416fae778a02b21
Khalid Omer <komer301@gmail.com>
2023-12-01 14:43:28 -0500

    optimized sentiment and created api in app

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-# Testing
+from flask import Flask, request
+from flask_restful import Api, Resource
+import sentiment
 
-#DOUble Testing
+app = Flask(__name__)
+api = Api(app)
+
+class SentimentAnalysis(Resource):
+    def get(self,videoID):
+        return sentiment.analysis(videoID)
+
+api.add_resource(SentimentAnalysis, "/<string:videoID>")
+if __name__ == "__main__":
+    app.run(debug=True)
--- sentiment.py
+++ sentiment.py
@@ -2,38 +2,49 @@ from googleapiclient.discovery import build
 import pandas as pd
 import config
 from transformers import pipeline
+import concurrent.futures
 
 api_service_name = "youtube"
 api_version = "v3"
 api_key = config.apiKey
+sent_pipeline = pipeline(model="lxyuan/distilbert-base-multilingual-cased-sentiments-student")
 
-def analysis(video_id):
+def fetch_comments(video_id):
     youtube = build('youtube', 'v3', developerKey=api_key)
-    # video_id = "SIm2W9TtzR0"
-    video_response=youtube.commentThreads().list(
+    video_response = youtube.commentThreads().list(
         part='snippet',
         videoId=video_id
-        ).execute()
-    comments = {}
-    sent_pipeline = pipeline(model="lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+    ).execute()
+    comments = []
     while video_response:
-            for item in video_response['items']:
-                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-                comments[comment] = sent_pipeline(comment)[0]['label']
-            if 'nextPageToken' in video_response:
-                video_response = youtube.commentThreads().list(
-                        part = 'snippet',
-                        videoId = video_id,
-                        pageToken = video_response['nextPageToken']
-                    ).execute()
-            else:
-                break
+        for item in video_response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay'][:500]
+            comments.append(comment)
+        if 'nextPageToken' in video_response:
+            video_response = youtube.commentThreads().list(
+                part='snippet',
+                videoId=video_id,
+                pageToken=video_response['nextPageToken']
+            ).execute()
+        else:
+            break
+    return comments
 
-    comments_df = pd.DataFrame.from_dict(comments,orient='index',columns=['sentiment']).reset_index()
-    comments_df.columns = ['comment', 'sentiment']
+def analyze_sentiment(comment):
+    return sent_pipeline(comment)[0]['label']
+
+def analysis(video_id):
+    comments = fetch_comments(video_id)
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        sentiment_labels = list(executor.map(analyze_sentiment, comments))
+
+    comments_df = pd.DataFrame({
+        'comment': comments,
+        'sentiment': sentiment_labels
+    })
+
     sentiment_distribution = comments_df['sentiment'].value_counts(normalize=True) * 100
     sentiment_json = sentiment_distribution.to_json()
+    
     return sentiment_json
-
-
-
